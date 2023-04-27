c6e3218e739666fe999932f3df0999effa199e3e
CHOP4NCHO <pancho.pantoja@outlook.es>
2023-04-27 17:40:05 -0400

    Agregar, modificar y eliminar objetos de tabla camión

--- python/manager.py
+++ python/manager.py
@@ -1,9 +1,10 @@
-import modules as m
+from modules import *
 from classes import *
 from sqlite3Manager import SQLite3Manager
 
 c = SQLite3Manager("database")
-m.insertTruckFromObject(c,Truck("camion1","sala-camiones"))
+
+
 
 
 
